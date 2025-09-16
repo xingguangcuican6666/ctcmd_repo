3e3c4a8066ce5f30194f8e4efa00a4040eed36bd
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-09-16 17:40:32 +0800

    upload

--- ctcmd_upload.bat
+++ ctcmd_upload.bat
@@ -10,27 +10,30 @@ if "%~4"=="" (
 )
 set "HOME=%~dp0"
 set "name=%2"
+@REM echo Name: %name%
+@REM goto 1
 set "arch=%3"
 set "version=%4"
+set found=0
 for /f "tokens=* delims=" %%a in ('type "%HOME%\all.ctcmd"') do (
     set "line=%%a"
     if not "!line!"=="" if not "!line:~0,2!"=="//" (
         for /f "tokens=1,2 delims=@" %%b in ("!line!") do (
-            @REM echo å¤„ç†è¡Œ: !line!
-            @REM echo @å‰éƒ¨åˆ†: %%b
-            @REM echo @åŽéƒ¨åˆ†: %%c
+            @REM echo ´¦ÀíÐÐ: !line!
+            @REM echo @Ç°²¿·Ö: %%b
+            @REM echo @ºó²¿·Ö: %%c
             @REM echo.
             if "%name%"=="%%b" (
                 echo Found matching name: %%b
-                goto skip
-            ) else (
-                echo %name%@https://raw.githubusercontent.com/xingguangcuican6666/ctcmd_repo/refs/heads/main/%name% >> "%HOME%\all.ctcmd"
-                goto skip
+                set found=1
             )
         )
     )
 )
 :skip
+if "%found%"=="0" (
+    echo %name%@https://raw.githubusercontent.com/xingguangcuican6666/ctcmd_repo/refs/heads/main/%name% >> "%HOME%\all.ctcmd"    
+)
 mkdir "%HOME%%name%"
 move /y "%HOME%%name%\%name%_%arch%_latest.zip" "%HOME%%name%\%name%_%arch%_%version%.zip"
 copy /y %1 "%HOME%%name%\%name%_%arch%_latest.zip"
@@ -41,5 +44,6 @@ git config --global user.name "xingguangcuican6666"
 git add . -v
 git commit -m "upload" -v
 git push -v
+:1
 cd /d %now_work%
 endlocal
